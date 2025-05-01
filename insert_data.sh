a8a44c32e576a6abe939f9684528e5dbf7701017
developedByAn009 <kreechanouar@gmail.com>
2025-05-01 01:32:35 +0000

    Save progress: 1.1

--- insert_data.sh
+++ insert_data.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 if [[ $1 == "test" ]]
 then
@@ -7,4 +7,28 @@ else
   PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
 fi
 
-# Do not change code above this line. Use the PSQL variable above to query your database.
+# Clear existing data
+echo $($PSQL "TRUNCATE teams, games RESTART IDENTITY CASCADE")
+
+# First insert all unique teams
+TEAMS=$(tail -n +2 games.csv | cut -d ',' -f 3,4 | tr ',' '\n' | sort | uniq)
+
+while IFS= read -r TEAM
+do
+  if [[ -n $TEAM ]]
+  then
+    # Insert team if it doesn't exist
+    echo $($PSQL "INSERT INTO teams(name) VALUES('$TEAM') ON CONFLICT (name) DO NOTHING")
+  fi
+done <<< "$TEAMS"
+
+# Then insert all games
+tail -n +2 games.csv | while IFS=',' read -r YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  # Get team IDs
+  WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+  OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+
+  # Insert game
+  echo $($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
+done
